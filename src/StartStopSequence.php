467aa058f99be19962adaabd9e5eae760c7f2d7d
appkit-framework <admin@appkit.biz>
2025-12-20 15:37:46 +0100

    Improve logging

--- src/StartStopSequence.php
+++ src/StartStopSequence.php
@@ -19,30 +19,27 @@ class StartStopSequence {
     }
     
     public function start() {
-        try {
-            for($i = 0; $i < count($this -> modules); $i++) {
-                $moduleName = get_class($this -> modules[$i]['module']);
-                $this -> log -> debug("Starting module $moduleName...");
-                try {
-                    $this -> modules[$i]['module'] -> start();
-                    $this -> modules[$i]['started'] = true;
-                } catch(CanceledException $e) {
-                    $this -> log -> debug("Start of module $moduleName has been canceled");
-                    throw $e;
-                } catch(Throwable $e) {
-                    $error = "Failed to start module $moduleName";
-                    $this -> log -> error($error, $e);
-                    throw new StartStopException(
-                        $error,
-                        previous: $e
-                    );
-                }
+        for($i = 0; $i < count($this -> modules); $i++) {
+            $moduleName = get_class($this -> modules[$i]['module']);
 
-                throwIfCanceled();
+            $this -> log -> debug("Starting module $moduleName...");
+            try {
+                $this -> modules[$i]['module'] -> start();
+                $this -> modules[$i]['started'] = true;
+                $this -> log -> info("Started module $moduleName");
+            } catch(CanceledException $e) {
+                $this -> log -> info("Start of module $moduleName canceled");
+                throw $e;
+            } catch(Throwable $e) {
+                $error = "Failed to start module $moduleName";
+                $this -> log -> error($error, $e);
+                throw new StartStopException(
+                    $error,
+                    previous: $e
+                );
             }
-        } catch(CanceledException $e) {
-            $this -> log -> debug('Start sequence has been canceled');
-            throw $e;
+
+            throwIfCanceled();
         }
     }
     
@@ -50,9 +47,11 @@ class StartStopSequence {
         for($i = count($this -> modules) - 1; $i >= 0; $i--) {
             if($this -> modules[$i]['started']) {
                 $moduleName = get_class($this -> modules[$i]['module']);
+
                 $this -> log -> debug("Stopping module $moduleName...");
                 try {
                     $this -> modules[$i]['module'] -> stop();
+                    $this -> log -> info("Stopped module $moduleName");
                 } catch(Throwable $e) {
                     $error = "Failed to stop module $moduleName";
                     $this -> log -> error($error, $e);
